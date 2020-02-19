cc520fada38254ee7215fec9b65ceb5aa0757359
aechavarris <56934671+aechavarris@users.noreply.github.com>
2020-02-19 13:16:56 +0100

    Primera actividad

---
+++ app/src/main/java/com/instantmusic/appmovil/MusicApp.java
@@ -0,0 +1,14 @@
+package com.instantmusic.appmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_app);
+    }
+}
