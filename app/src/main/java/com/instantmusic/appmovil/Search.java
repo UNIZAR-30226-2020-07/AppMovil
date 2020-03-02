3f65c0550ae7f9aed4b116658872d38d60fb88d2
aechavarris <56934671+aechavarris@users.noreply.github.com>
2020-03-02 22:55:32 +0100

    Solucionando compatibilidad con android 4

--- app/src/main/java/com/instantmusic/appmovil/Search.java
+++ app/src/main/java/com/instantmusic/appmovil/Search.java
@@ -1,6 +1,6 @@
 package com.instantmusic.appmovil;
 
-import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,19 +8,12 @@ import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ListView;
-import com.instantmusic.appmovil.localServer;
-import android.database.Cursor;
-import android.widget.ScrollView;
 import android.widget.SimpleCursorAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.net.UnknownServiceException;
-import java.util.List;
-
-public class Login extends AppCompatActivity {
+public class Search extends AppCompatActivity {
     private ListView resList;
-    private View resElement;
     private static final int SEARCH = Menu.FIRST;
     private static final int RECOVER = Menu.FIRST+1;
     private static final int LOGIN = Menu.FIRST + 2;
@@ -29,10 +22,22 @@ public class Login extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_instant_music_app_login);
+        setContentView(R.layout.activity_instant_music_app_search);
         resList =findViewById(R.id.searchRes);
         server=new localServer(this);
 //        registerForContextMenu(resList);
+        shit = findViewById(R.id.searchbar);
+        Cursor shitCursor = server.searchShit(shit.getText().toString());
+        startManagingCursor(shitCursor);
+
+        // Create an array to specify the fields we want to display in the list (only TITLE)
+        String[] from = new String[]{UsersDbAdapter.KEY_NAME, UsersDbAdapter.KEY_ARTIST, UsersDbAdapter.KEY_CATEGORY};
+
+        // and an array of the fields we want to bind those fields to (in this case just text1)
+        int[] to = new int[]{R.id.text1, R.id.text2,R.id.text3};
+        SimpleCursorAdapter search =
+                new SimpleCursorAdapter(this, R.layout.search_row, shitCursor, from, to);
+        resList.setAdapter(search);
         Button confirmButton = findViewById(R.id.search);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -48,9 +53,18 @@ public class Login extends AppCompatActivity {
     }
 
     private void confirmSearch() {
-        Intent i = new Intent(this, Search.class);
-        startActivityForResult(i, 0);
+        shit = findViewById(R.id.searchbar);
+        Cursor shitCursor = server.searchShit(shit.getText().toString());
+        startManagingCursor(shitCursor);
 
+        // Create an array to specify the fields we want to display in the list (only TITLE)
+        String[] from = new String[]{UsersDbAdapter.KEY_NAME, UsersDbAdapter.KEY_ARTIST, UsersDbAdapter.KEY_CATEGORY};
+
+        // and an array of the fields we want to bind those fields to (in this case just text1)
+        int[] to = new int[]{R.id.text1, R.id.text2,R.id.text3};
+        SimpleCursorAdapter search =
+                new SimpleCursorAdapter(this, R.layout.search_row, shitCursor, from, to);
+        resList.setAdapter(search);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
