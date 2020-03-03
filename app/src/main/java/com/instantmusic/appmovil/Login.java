8a1fbe505e03e39381121e997632cbdf6770c484
aechavarris <56934671+aechavarris@users.noreply.github.com>
2020-03-03 13:09:25 +0100

    Estoy con la busqueda

--- app/src/main/java/com/instantmusic/appmovil/Login.java
+++ app/src/main/java/com/instantmusic/appmovil/Login.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import com.instantmusic.appmovil.localServer;
 import android.database.Cursor;
@@ -20,10 +21,9 @@ import java.util.List;
 
 public class Login extends AppCompatActivity {
     private ListView resList;
+    private String stringSearch;
     private View resElement;
     private static final int SEARCH = Menu.FIRST;
-    private static final int RECOVER = Menu.FIRST+1;
-    private static final int LOGIN = Menu.FIRST + 2;
     private AutoCompleteTextView shit;
     private serverInterface server;
     public void onCreate(Bundle savedInstanceState) {
@@ -33,6 +33,8 @@ public class Login extends AppCompatActivity {
         resList =findViewById(R.id.searchRes);
         server=new localServer(this);
 //        registerForContextMenu(resList);
+        EditText searchBar=findViewById(R.id.searchbar);
+        stringSearch=searchBar.getText().toString();
         Button confirmButton = findViewById(R.id.search);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -43,7 +45,12 @@ public class Login extends AppCompatActivity {
 
     private void confirmSearch() {
         Intent i = new Intent(this, Search.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("search",stringSearch);
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        i.putExtras(bundle);
         startActivityForResult(i, 0);
+
 
     }
 
--- app/src/main/java/com/instantmusic/appmovil/Search.java
+++ app/src/main/java/com/instantmusic/appmovil/Search.java
@@ -15,19 +15,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Search extends AppCompatActivity {
     private ListView resList;
+    private String stringSearch;
     private static final int SEARCH = Menu.FIRST;
-    private static final int RECOVER = Menu.FIRST+1;
+    private static final int RECOVER = Menu.FIRST + 1;
     private static final int LOGIN = Menu.FIRST + 2;
     private AutoCompleteTextView shit;
     private serverInterface server;
+
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instant_music_app_search);
-        resList =findViewById(R.id.searchRes);
-        server=new localServer(this);
+        resList = findViewById(R.id.searchRes);
+        Bundle datos = this.getIntent().getExtras();
+        stringSearch = datos.getString("search");
+        server = new localServer(this);
 //        registerForContextMenu(resList);
-        shit = findViewById(R.id.searchbar);
+        shit = findViewById(R.id.searchbar2);
+        shit.setText(stringSearch);
         Cursor shitCursor = server.searchShit(shit.getText().toString());
         startManagingCursor(shitCursor);
 
@@ -35,7 +40,7 @@ public class Search extends AppCompatActivity {
         String[] from = new String[]{UsersDbAdapter.KEY_NAME, UsersDbAdapter.KEY_ARTIST, UsersDbAdapter.KEY_CATEGORY};
 
         // and an array of the fields we want to bind those fields to (in this case just text1)
-        int[] to = new int[]{R.id.text1, R.id.text2,R.id.text3};
+        int[] to = new int[]{R.id.text1, R.id.text2, R.id.text3};
         SimpleCursorAdapter search =
                 new SimpleCursorAdapter(this, R.layout.search_row, shitCursor, from, to);
         resList.setAdapter(search);
@@ -52,12 +57,13 @@ public class Search extends AppCompatActivity {
             }
         });
     }
+
     @Override
-    public void onBackPressed()
-    {
-        Intent intent = new Intent(this,Login.class);
+    public void onBackPressed() {
+        Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
+
     private void confirmSearch() {
         shit = findViewById(R.id.searchbar);
         Cursor shitCursor = server.searchShit(shit.getText().toString());
@@ -67,7 +73,7 @@ public class Search extends AppCompatActivity {
         String[] from = new String[]{UsersDbAdapter.KEY_NAME, UsersDbAdapter.KEY_ARTIST, UsersDbAdapter.KEY_CATEGORY};
 
         // and an array of the fields we want to bind those fields to (in this case just text1)
-        int[] to = new int[]{R.id.text1, R.id.text2,R.id.text3};
+        int[] to = new int[]{R.id.text1, R.id.text2, R.id.text3};
         SimpleCursorAdapter search =
                 new SimpleCursorAdapter(this, R.layout.search_row, shitCursor, from, to);
         resList.setAdapter(search);
@@ -76,10 +82,10 @@ public class Search extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case SEARCH:
-                shit=findViewById(R.id.searchbar);
+                shit = findViewById(R.id.searchbar);
                 server.searchShit(shit.getText().toString());
                 return true;
-            
+
         }
         return super.onOptionsItemSelected(item);
     }
