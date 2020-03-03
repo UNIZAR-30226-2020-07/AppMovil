999dea1e765ac162bb8557f163113c511803ba35
aechavarris <56934671+aechavarris@users.noreply.github.com>
2020-03-03 16:03:06 +0100

    Buscar lupa.jpg mas grande

--- app/src/main/java/com/instantmusic/appmovil/Search.java
+++ app/src/main/java/com/instantmusic/appmovil/Search.java
@@ -8,8 +8,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +20,9 @@ public class Search extends AppCompatActivity {
     private ListView resList;
     private Cursor shitCursor;
     private String stringSearch;
+    private TextView searchTip1;
+    private TextView searchTip2;
+    private ImageView lupaGrande;
     private static final int SEARCH = Menu.FIRST;
     private static final int RECOVER = Menu.FIRST + 1;
     private static final int LOGIN = Menu.FIRST + 2;
@@ -28,6 +34,9 @@ public class Search extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instant_music_app_search);
         resList = findViewById(R.id.searchRes);
+        searchTip1=findViewById(R.id.searchTip1);
+        searchTip2=findViewById(R.id.searchTip2);
+        lupaGrande=findViewById(R.id.lupaGrande);
         Bundle datos = this.getIntent().getExtras();
         stringSearch = datos.getString("search");
         server = new localServer(this);
@@ -64,19 +73,30 @@ public class Search extends AppCompatActivity {
     }
 
     private void confirmSearch() {
+
         shit = findViewById(R.id.searchbar2);
-        shitCursor = server.searchShit(shit.getText().toString());
-        System.out.println("debug");
-        startManagingCursor(shitCursor);
+        if(shit.getText().toString()==null){
+            searchTip1.setVisibility(View.VISIBLE);
+            searchTip2.setVisibility(View.VISIBLE);
+            lupaGrande.setVisibility(View.VISIBLE);
+        }else {
+            searchTip1.setVisibility(View.INVISIBLE);
+            searchTip2.setVisibility(View.INVISIBLE);
+            lupaGrande.setVisibility(View.INVISIBLE);
 
-        // Create an array to specify the fields we want to display in the list (only TITLE)
-        String[] from = new String[]{UsersDbAdapter.KEY_NAME, UsersDbAdapter.KEY_ARTIST, UsersDbAdapter.KEY_CATEGORY};
+            shitCursor = server.searchShit(shit.getText().toString());
+            System.out.println("debug");
+            startManagingCursor(shitCursor);
 
-        // and an array of the fields we want to bind those fields to (in this case just text1)
-        int[] to = new int[]{R.id.text1, R.id.text2, R.id.text3};
-        SimpleCursorAdapter search =
-                new SimpleCursorAdapter(this, R.layout.search_row, shitCursor, from, to);
-        resList.setAdapter(search);
+            // Create an array to specify the fields we want to display in the list (only TITLE)
+            String[] from = new String[]{UsersDbAdapter.KEY_NAME, UsersDbAdapter.KEY_ARTIST, UsersDbAdapter.KEY_CATEGORY};
+
+            // and an array of the fields we want to bind those fields to (in this case just text1)
+            int[] to = new int[]{R.id.text1, R.id.text2, R.id.text3};
+            SimpleCursorAdapter search =
+                    new SimpleCursorAdapter(this, R.layout.search_row, shitCursor, from, to);
+            resList.setAdapter(search);
+        }
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
