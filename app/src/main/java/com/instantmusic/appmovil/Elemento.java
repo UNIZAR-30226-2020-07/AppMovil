308876f9810f9505f2218cf50c4217e09a36feaf
aechavarris <56934671+aechavarris@users.noreply.github.com>
2020-03-17 19:08:25 +0100

    Playlists horizontales compila y ejecuta pero no estan testeados

---
+++ app/src/main/java/com/instantmusic/appmovil/Elemento.java
@@ -0,0 +1,9 @@
+package com.instantmusic.appmovil;
+
+abstract class Elemento{
+    protected String name;
+    protected String artist;
+
+    abstract public String getName();
+    abstract public String getArtist();
+}
