a0cf39464a1806551fac169e22208a46e3f1b42b
Markles <61208918+Markles01@users.noreply.github.com>
2020-03-17 18:26:51 +0100

    Avances pequeños

--- app/src/main/java/com/instantmusic/appmovil/Elemento.java
+++
@@ -1,9 +0,0 @@
-package com.instantmusic.appmovil;
-
-abstract class Elemento{
-    protected String name;
-    protected String artist;
-
-    abstract public String getName();
-    abstract public String getArtist();
-}
