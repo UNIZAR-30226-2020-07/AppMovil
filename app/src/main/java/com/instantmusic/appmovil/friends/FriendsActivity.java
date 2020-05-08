4fe717e0309e90981b517c67df5d6cd4e6e70a9d
Markles <61208918+Markles01@users.noreply.github.com>
2020-05-08 13:31:03 +0200

    Cambios en Friends

--- app/src/main/java/com/instantmusic/appmovil/friends/FriendsActivity.java
+++ app/src/main/java/com/instantmusic/appmovil/friends/FriendsActivity.java
@@ -1,38 +1,26 @@
 package com.instantmusic.appmovil.friends;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.instantmusic.appmovil.IntentTransfer;
 import com.instantmusic.appmovil.R;
-import com.instantmusic.appmovil.album.Album;
-import com.instantmusic.appmovil.artist.ArtistActivity;
 import com.instantmusic.appmovil.playlist.Playlist;
 import com.instantmusic.appmovil.playlist.PlaylistActivity;
 import com.instantmusic.appmovil.playlist.PlaylistAdapter;
 import com.instantmusic.appmovil.server.connect.JSONConnection;
 import com.instantmusic.appmovil.server.remoteServer;
 import com.instantmusic.appmovil.server.serverInterface;
-import com.instantmusic.appmovil.song.Song;
-import com.instantmusic.appmovil.song.SongActivity;
-import com.instantmusic.appmovil.song.SongsAdapter;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.Comparator;
 
 public class FriendsActivity extends AppCompatActivity {
     private ListView resList;
@@ -43,6 +31,7 @@ public class FriendsActivity extends AppCompatActivity {
     private PlaylistAdapter adapterPlaylist;
 
 
+    @SuppressLint("SetTextI18n")
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instant_music_app_friends);
@@ -57,7 +46,7 @@ public class FriendsActivity extends AppCompatActivity {
             name.setText(username);
             friendPlaylist.setText(username+"'s");
         }
-        adapterPlaylist = new PlaylistAdapter(this, arrayOfPlaylists,0);
+        adapterPlaylist = new PlaylistAdapter(this, arrayOfPlaylists,2);
         resList.setAdapter(adapterPlaylist);
         server.getUserById(id, new JSONConnection.Listener() {
             @Override
--- app/src/main/java/com/instantmusic/appmovil/playlist/PlaylistAdapter.java
+++ app/src/main/java/com/instantmusic/appmovil/playlist/PlaylistAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.instantmusic.appmovil.R;
@@ -27,8 +28,12 @@ public class PlaylistAdapter extends ArrayAdapter<Playlist> {
         if (convertView == null) {
             if (this.tipoLayout == 0) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.myplaylists_row, parent, false);
-            } else if (this.tipoLayout == 1) {
+            }
+            else if (this.tipoLayout == 1) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.myplaylists2_row, parent, false);
+            }
+            else if (this.tipoLayout == 2 ) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.search_row3, parent, false);
             }
         }
         // Lookup view for data population
@@ -36,6 +41,11 @@ public class PlaylistAdapter extends ArrayAdapter<Playlist> {
         // Populate the data into the template view using the data object
 
         playlistName.setText(playlist.playlistName);
+
+        if ( this.tipoLayout == 2 ) {
+            ImageView categoryImage = convertView.findViewById(R.id.iconoSong);
+            categoryImage.setImageDrawable(getContext().getResources().getDrawable(R.drawable.rufio));
+        }
 
         // Return the completed view to render on screen
         return convertView;
